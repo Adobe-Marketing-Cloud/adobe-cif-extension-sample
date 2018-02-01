104b31fc5df385dbfe4d8851d28198ea4f4ccd0d
Markus Haack <mhaack@adobe.com>
2018-02-01 15:00:42 +0200

    Make exercise-03 working

--- exercise-03/applyDiscount.js
+++ exercise-03/applyDiscount.js
@@ -1,35 +1,11 @@
-/* Markus Haack - TBA */
-function main(params, args) {
-
-   var allVariants = params.variants;
-   var variantFound = false;
-   var discountToApply = 20;
-   var discountedPrice = "Please refer into store for price !!";
-   var arguement = "No Arguement";
-
-   if(args != null && args.length > 0) {
-   		arguement = args[0];
-   }
+function main(params) {
+   var discountToApply = 0.05; // 5% discount
    
-   for (i in allVariants) {
-
-   		if(allVariants[i].id == "526dc571-104f-40fb-b761-71781a97910b-2") {
-   			variantFound = true;
-
-   			var allVariantsPrices = allVariants[i].prices;
-
-   			if(allVariantsPrices.length > 0) {
-   				discountedPrice = allVariantsPrices[0].centAmount;
-
-	   			if(discountedPrice != null && discountedPrice > discountToApply) {
-	   				discountedPrice = discountedPrice - discountToApply;
-	   			}
-
-   			}
-
-   		}
-
-   }
-
-   return {discountedPrice:  discountedPrice, args: arguement};
+   params.response.body.variants.map(variant => {
+		return variant.prices.map(price => {
+			price.centAmount -= price.centAmount * discountToApply;
+			return price;
+		})
+	})
+   return params;
 }
